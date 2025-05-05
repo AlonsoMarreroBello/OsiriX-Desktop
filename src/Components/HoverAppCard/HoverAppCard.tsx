f8aa458fe1a493320df91c9bcc52c501971ca200
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-05 03:38:55 +0200

    refact(HoverAppCard):  Improved the style to show the information approperly

--- src/Components/HoverAppCard/HoverAppCard.tsx
+++ src/Components/HoverAppCard/HoverAppCard.tsx
@@ -4,36 +4,35 @@ import style from "./HoverAppCard.module.css";
 interface HoverAppCardProps {
   app: AppInfo;
   className?: string;
+  inlineStyle?: React.CSSProperties;
 }
 
-const HoverAppCard = ({ app, className }: HoverAppCardProps) => {
+const HoverAppCard = ({ app, className, inlineStyle }: HoverAppCardProps) => {
   return (
-    <div className={`${style.container} ${className || ""}`}>
-      {/* Usamos app.imgUrl para el banner/imagen principal */}
-      <img src={app.imgUrl} alt={`${app.name} banner`} className={style.banner} loading="lazy" />
+    <div className={`${style.container} ${className || ""}`} style={inlineStyle}>
       <div className={style.contentWrapper}>
+        <img src={app.imgUrl} alt={`${app.name} banner`} className={style.banner} />
         <h1 className={style.title}>{app.name}</h1>
         <p className={style.description}>{app.description || "Descripción no disponible."}</p>
         <div className={style.metaDataGrid}>
           {/* Categorías */}
           {app.categories && app.categories.length > 0 && (
             <div className={style.metaDataItem}>
-              <Category className={style.metaDataIcon} fontSize="small" />
+              <Category className={style.metaDataIcon} />
               <span className={style.metaDataText}>{app.categories.join(", ")}</span>
             </div>
           )}
-          {/* Contador de Descargas (Ejemplo) */}
+          {/* Contador de Descargas */}
           {app.downloads !== undefined && (
             <div className={style.metaDataItem}>
-              <Download className={style.metaDataIcon} fontSize="small" />
-              {/* Puedes formatear el número si quieres */}
+              <Download className={style.metaDataIcon} />
               <span className={style.metaDataText}>{app.downloads.toLocaleString()}</span>
             </div>
           )}
-          {/* Fecha (Ejemplo) */}
+          {/* Fecha  */}
           {app.releaseDate && (
             <div className={style.metaDataItem}>
-              <CalendarMonth className={style.metaDataIcon} fontSize="small" />
+              <CalendarMonth className={style.metaDataIcon} />
               <span className={style.metaDataText}>{app.releaseDate.toString()}</span>
             </div>
           )}
