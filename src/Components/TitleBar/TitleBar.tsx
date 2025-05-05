32e87d37f79db9b0027bad843a4c1d669121bab2
sandysan <user3858@salesianos-lacuesta.net>
2025-05-05 13:56:46 +0100

    refact(TitleBar): Improved style of the main bar

---
+++ src/Components/TitleBar/TitleBar.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import style from "./TitleBar.module.css";
+import logo from "../../assets/bobCholo.jpeg"
+import { AddBoxOutlined, ArrowCircleUp, ArrowUpward, PeopleAlt, Person2, PublishOutlined, Settings } from "@mui/icons-material";
+export default function TitleBar() {
+  const [isMaximized, setIsMaximized] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.electron.isMaximized(setIsMaximized);
+  }, []);
+
+  return (
+    <div className={style.titleBar}>
+      <div className={style.actionsContainer}>
+        <div className={style.linkActions}>
+          <div className={style.actionInnerContainer}>
+            <img src={logo} alt="logo" className={style.logo} />
+            <p className={style.actionText}>Osirix</p>
+          </div>
+          <div className={style.actionInnerContainer}>
+            <Settings className={style.icon} />
+            <button><p className={style.actionText}>Ajustes</p></button>
+          </div>
+          <div className={style.actionInnerContainer}>
+            <PeopleAlt className={style.icon} />
+            <button><p className={style.actionText}>Amigos</p></button>
+          </div>
+          <div className={style.actionInnerContainer}>
+            <AddBoxOutlined className={style.icon} />
+            <button><p className={style.actionText}>Canjear</p></button>
+          </div>
+          <div className={style.actionInnerContainer}>
+            <PublishOutlined className={style.icon} />
+            <button><p className={style.actionText}>Subir una aplicación</p></button>
+          </div>
+        </div>
+
+        <div className={style.windowActions}>
+          <button onClick={() => window.electron.minimize()}>➖</button>
+          <button
+            onClick={() => {
+              window.electron.maximize();
+              setIsMaximized(!isMaximized);
+            }}
+          >
+            {isMaximized ? "🗗" : "🗖"}
+          </button>
+          <button onClick={() => window.electron.close()}>❌</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -2,11 +2,15 @@ import type { Configuration } from 'webpack';
 
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
-
+const path = require('path');
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+},
+  {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    type: 'asset/resource',
+  });
 
 export const rendererConfig: Configuration = {
   module: {
@@ -15,5 +19,8 @@ export const rendererConfig: Configuration = {
   plugins,
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    }
   },
 };
