e99d72c25f6317059493bd14a563a691e22b3c02
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-05 03:29:14 +0200

    refact(BasicAppCard): Changed the way that the hover is displayed

--- src/Components/BasicAppCard/BasicAppCard.tsx
+++ src/Components/BasicAppCard/BasicAppCard.tsx
@@ -1,22 +1,28 @@
 import AppInfo from "../../types/AppInfo";
-import HoverAppCard from "../HoverAppCard/HoverAppCard";
 import styles from "./BasicAppCard.module.css";
 
 interface BasicAppCardProp {
   app: AppInfo;
+  onHover?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>, app: AppInfo) => void;
+  onLeave?: () => void;
 }
 
-const BasicAppCard = ({ app }: BasicAppCardProp) => {
+const BasicAppCard = ({ app, onHover, onLeave }: BasicAppCardProp) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-        <img src={app.imgUrl} alt={`${app.name} icon`} className={styles.icon} loading="lazy" />
+    <a href="https://guthib.com/" className={styles.mainLinkArea}>
+      <div
+        className={styles.container}
+        onMouseEnter={(e) => onHover(e, app)}
+        onMouseLeave={() => onLeave()}
+      >
+        <div className={styles.contentWrapper}>
+          <img src={app.imgUrl} alt={`${app.name} icon`} className={styles.icon} loading="lazy" />
+        </div>
+        <div className={styles.titleContainer}>
+          <p className={styles.title}>{app.name} </p>
+        </div>
       </div>
-      <div className={styles.titleContainer}>
-        <p className={styles.title}>{app.name} </p>
-      </div>
-      <HoverAppCard app={app} className={styles.hoverCard} />
-    </div>
+    </a>
   );
 };
 
