0d76202c262c4b9a04dd4aeae5be4b5dc78abbf1
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-07 17:14:42 +0200

    feat(Header): Added navigations through tabs

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import { NavigateBefore, NavigateNext, Replay } from "@mui/icons-material";
 import style from "./Header.module.css";
 import { Avatar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   username: string;
 }
 
 const Header = ({ username }: HeaderProps) => {
+  const navigate = useNavigate();
   return (
     <div className={style.container}>
       <div className={style.actionWrapper}>
@@ -20,10 +22,10 @@ const Header = ({ username }: HeaderProps) => {
           <button className={style.navigationButton}>
             <NavigateNext className={style.icon} />
           </button>
-          <button className={style.navigationButton}>
+          <button className={style.navigationButton} onClick={() => navigate("/home")}>
             <p className={style.navigationText}>Tienda</p>
           </button>
-          <button className={style.navigationButton}>
+          <button className={style.navigationButton} onClick={() => navigate("/library")}>
             <p className={style.navigationText}>Biblioteca</p>
           </button>
         </div>
