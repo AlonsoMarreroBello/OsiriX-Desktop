a52a8df477e29870794d5416bef3ee38919e26df
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-07 17:13:02 +0200

    feat(): Added initial routes

--- src/Components/app.tsx
+++ src/Components/app.tsx
@@ -1,15 +1,21 @@
 import TitleBar from "./TitleBar/TitleBar";
 import Header from "./Header/Header";
 import HomePage from "../pages/home/HomePage";
+import { Route, Routes } from "react-router-dom";
+import LibraryPage from "../pages/library/LibraryPage";
 
 const App = () => {
+  // implementar luego la comprobación si el usuario está logueado o no, en base al token almacenado en el localStorage
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <TitleBar />
       <Header username="TuPrima" />
-      <div>
-        <HomePage />
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app";
+import { StrictMode } from "react";
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.body);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
