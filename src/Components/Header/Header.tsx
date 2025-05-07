c429f9c16a28fb3612187d73a91994d43e8ae437
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-07 05:42:36 +0200

    refact(Header): Improved code index

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,28 +1,46 @@
 import { NavigateBefore, NavigateNext, Replay } from "@mui/icons-material";
-import style from "./Header.module.css"
+import style from "./Header.module.css";
+import { Avatar } from "@mui/material";
 
 interface HeaderProps {
-    username: string;
+  username: string;
 }
 
 const Header = ({ username }: HeaderProps) => {
-    return (
-        <div className={style.container}>
-            <div className={style.actionWrapper}>
-                <div className={style.navigationActionsContainer}>
-                    <Replay className={style.icon} />
-                    <NavigateBefore className={style.icon} />
-                    <NavigateNext className={style.icon} />
-                    <button className={style.navigationButton}><p className={style.navigationText}>Tienda</p></button>
-                    <button className={style.navigationButton}><p className={style.navigationText}>Biblioteca</p></button>
-                </div>
-                <div className={style.userContainer}>
-                    <p className={style.username}>{username}</p>
-                    <button className={style.userAvatar}>{username.charAt(0).toUpperCase()}</button>
-                </div>
-            </div>
+  return (
+    <div className={style.container}>
+      <div className={style.actionWrapper}>
+        <div className={style.navigationActionsContainer}>
+          <button className={style.navigationButton}>
+            <Replay className={style.icon} />
+          </button>
+          <button className={style.navigationButton}>
+            <NavigateBefore className={style.icon} />
+          </button>
+          <button className={style.navigationButton}>
+            <NavigateNext className={style.icon} />
+          </button>
+          <button className={style.navigationButton}>
+            <p className={style.navigationText}>Tienda</p>
+          </button>
+          <button className={style.navigationButton}>
+            <p className={style.navigationText}>Biblioteca</p>
+          </button>
         </div>
-    )
-}
+        <div className={style.userContainer}>
+          <p className={style.username}>{username}</p>
+          <Avatar
+            sx={{
+              bgcolor: "var(--bg-primary)",
+              border: "2px solid var(--text-primary)",
+            }}
+          >
+            {username.charAt(0)}
+          </Avatar>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
