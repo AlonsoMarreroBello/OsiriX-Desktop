a0f6bacaf8a24dbbca8173891a52a3fc8b9259a1
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2025-05-07 17:15:38 +0200

    refact(HomePage): Changed logic about the hover card

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,90 +1,52 @@
-import { useEffect, useRef, useState } from "react";
-import hoverCardStyle from "../../components/HoverAppCard/HoverAppCard.module.css";
-import AppInfo from "../../interfaces/AppInfo";
-import HoverAppCard from "../../components/HoverAppCard/HoverAppCard";
+import { useEffect, useState } from "react";
 import { dummyAppData } from "../../data/FakeData";
 import AppList from "../../components/AppList/AppList";
-
-interface CardPosition {
-  top: number;
-  left: number;
-}
+import FilterBar, { ViewMode } from "../../components/FilterBar/FilterBar";
+import style from "./HomePage.module.css";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const HomePage = () => {
-  const [hoveredApp, setHoveredApp] = useState<AppInfo | null>(null);
-  const [initialCardPosition, setInitialCardPosition] = useState<CardPosition | null>(null);
-  const [adjustedCardPosition, setAdjustedCardPosition] = useState<CardPosition | null>(null);
-  const hoverCardRef = useRef<HTMLDivElement>(null);
+  const [viewMode, setViewMode] = useState<ViewMode>("lista");
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLElement>, app: AppInfo) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const top = rect.top + window.scrollY - 20;
-    const left = rect.right + window.scrollX + 20;
-
-    setHoveredApp(app);
-    setInitialCardPosition({ top, left });
-    setAdjustedCardPosition({ top, left });
+  const handleSearchTerm = (term: string) => {
+    setSearchTerm(term);
   };
 
-  const handleMouseLeave = () => {
-    setHoveredApp(null);
-    setInitialCardPosition(null);
-    setAdjustedCardPosition(null);
+  const search = () => {
+    console.log("Buscando: ", searchTerm);
+  };
+  const handleViewModeChange = (mode: ViewMode) => {
+    setViewMode(mode);
   };
 
-  useEffect(() => {
-    if (hoveredApp && initialCardPosition && hoverCardRef.current) {
-      const cardElement = hoverCardRef.current;
-      const cardHeight = cardElement.offsetHeight;
-      const viewportHeight = window.innerHeight;
-
-      let newTop = initialCardPosition.top;
-      const newLeft = initialCardPosition.left;
-
-      const margin = 20;
-
-      // Posición actual del tope de la pagina y el bottom acorde a la posicion de la tajertita
-      const currentCardTopPage = initialCardPosition.top;
-      const currentCardBottomPage = currentCardTopPage + cardHeight;
-
-      // Límite inferior visible del viewport y el límite superior visible del viewport de la página
-      const viewportBottomPage = window.scrollY + viewportHeight - margin;
-      const viewportTopPage = window.scrollY + margin;
-
-      if (currentCardBottomPage > viewportBottomPage) {
-        newTop = viewportBottomPage - cardHeight;
-      }
-      if (newTop < viewportTopPage) {
-        newTop = viewportTopPage;
-      }
-
-      // Evitar re renderizados en caso de que no haya cambios en la posicion..
-      if (newTop !== adjustedCardPosition?.top || newLeft !== adjustedCardPosition?.left) {
-        setAdjustedCardPosition({ top: newTop, left: newLeft });
-      }
+  const checkViewMode = () => {
+    if (viewMode === "mosaico") {
+      return style.appMosaicList;
+    } else if (viewMode === "lista") {
+      return style.appOtherList;
     }
-  }, [hoveredApp, initialCardPosition]);
+  };
+  useEffect(() => {
+    checkViewMode();
+  }, [viewMode]);
 
   return (
-    <div>
-      <AppList
-        apps={dummyAppData}
-        handleMouseEnter={handleMouseEnter}
-        handleMouseLeave={handleMouseLeave}
-      />
-      {hoveredApp && adjustedCardPosition && (
-        <HoverAppCard
-          app={hoveredApp}
-          className={hoverCardStyle.containerVisible}
-          inlineStyle={{
-            position: "absolute",
-            top: `${adjustedCardPosition.top}px`,
-            left: `${adjustedCardPosition.left}px`,
-            zIndex: 1000,
-          }}
-          ref={hoverCardRef}
+    <div className={style.homePageContainer}>
+      <div className={style.searchBarContainer}>
+        <SearchBar
+          placeholder="Buscar aplicaciones"
+          onSearch={search}
+          value={searchTerm}
+          onChange={(e) => handleSearchTerm(e.currentTarget.value)}
         />
-      )}
+      </div>
+      <div className={style.filterControlsContainer}>
+        <FilterBar handleViewMode={handleViewModeChange} />
+      </div>
+      <div className={style.appListContainer}>
+        <AppList apps={dummyAppData} checkViewMode={checkViewMode} viewMode={viewMode} />
+      </div>
     </div>
   );
 };
